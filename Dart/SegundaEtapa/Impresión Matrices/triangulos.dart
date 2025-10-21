02d8f5c2c15de7921ed7e02f23af83e0029d9424
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-10-21 07:29:21 -0700

    Reformateando el code

--- Dart/SegundaEtapa/Impresión Matrices/triangulos.dart
+++ Dart/SegundaEtapa/Impresión Matrices/triangulos.dart
@@ -1,33 +1,28 @@
 import 'dart:io';
 
-void main (){
-
+void main() {
   int filas = 5;
   int columnas = 5;
 
-  List<List<String>> Matriz = List.generate(filas, (_) => List.filled(columnas, '0'));
+  List<List<String>> Matriz =
+      List.generate(filas, (_) => List.filled(columnas, '0'));
 
-  for(int x = 0; x < filas ; x++){
-    for(int y = 0; y < columnas ; y++){
-      if(x == y || x + y == columnas - 1 ){
+  for (int x = 0; x < filas; x++) {
+    for (int y = 0; y < columnas; y++) {
+      if (x == y || x + y == columnas - 1) {
         Matriz[x][y] = "#";
-      }
-      else if(x  < y && x + y < columnas - 1){
+      } else if (x < y && x + y < columnas - 1) {
         Matriz[x][y] = 'A';
-      }
-      else if(x < y && x + y > columnas - 1){
+      } else if (x < y && x + y > columnas - 1) {
         Matriz[x][y] = "B";
-      }
-      else if(x > y && x + y < columnas - 1){
+      } else if (x > y && x + y < columnas - 1) {
         Matriz[x][y] = "C";
-      }
-      else {
+      } else {
         Matriz[x][y] = "D";
       }
     }
-    
-    }
-    for(var fila in Matriz){
-      print(fila.join(" "));
+  }
+  for (var fila in Matriz) {
+    print(fila.join(" "));
   }
 }
